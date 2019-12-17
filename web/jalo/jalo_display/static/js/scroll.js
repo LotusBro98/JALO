ed3b175ea1a31cbe2dddb81547abb892a24ce633
Alex Pan <LotusBro98@yandex.ru>
2019-12-17 19:29:20 +0000

    Shamaning on cobrain with web

--- web/jalo/jalo_display/static/js/scroll.js
+++ web/jalo/jalo_display/static/js/scroll.js
@@ -4,16 +4,18 @@ function sleep(ms) {
 
 async function runScroll(){
 
-    var timeout = 12000;
-
-    while (true) {
-        window.scroll(0, document.getElementById("slide2").offsetTop);
-        await sleep(timeout);
-        window.scroll(0, document.getElementById("slide3").offsetTop);
-        await sleep(timeout);
-        window.scroll(0, document.getElementById("slide4").offsetTop);
-        await sleep(timeout);
+    // while (true) {
         window.scroll(0, document.getElementById("slide1").offsetTop);
-        await sleep(timeout);
-    }
+        await sleep(10000);
+        window.scroll(0, document.getElementById("slide2").offsetTop);
+        await sleep(15000);
+        window.scroll(0, document.getElementById("slide3").offsetTop);
+        await sleep(30000);
+        window.scroll(0, document.getElementById("slide4").offsetTop);
+        await sleep(15000);
+        window.scroll(0, document.getElementById("slide5").offsetTop);
+        await sleep(10000);
+        window.scroll(0, document.getElementById("slide1").offsetTop);
+        // window.location.reload(true);
+    // }
 }
